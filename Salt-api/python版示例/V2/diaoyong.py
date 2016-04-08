db28a9be89801cca520e7483a812198ca2f200ee
knight <624867243@qq.com>
2016-04-08 21:03:41 +0800

    4444444444445555

--- Salt-api/python版示例/V2/diaoyong.py
+++ Salt-api/python版示例/V2/diaoyong.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 #coding:utf8
 from saltapi import *
-import saltapi
+#import saltapi
 
 sapi = saltAPI()
-#定义params
-params = {'client':'local', 'fun':'test.ping', 'tgt':'*'}
+#params = {'client':'local', 'fun':'test.ping', 'tgt':'*'}
+
+#params = {'client':'local','tgt':'*', 'fun':'cmd.run', 'arg1':'hello'}
+
+#params = {'client':'local','tgt':'*', 'fun':'cmd.run', 'arg1':'hostname'}
+params = {'client':'local','tgt':'*', 'fun':'cmd.run', 'arg1':'touch /root/cc.txt;touch cc1.txt'}
+
 test = sapi.saltCmd(params)
 #test = sapi.saltCmd()
 print test
